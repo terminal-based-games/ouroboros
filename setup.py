27b9b848ecfe3f2ce226e77ade08523067c0d2d7
Carissa Allen <carissawood@outlook.com>
2019-07-12 21:16:13 -0700

    Correctly format setup file

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@
 from distutils.core import setup
 
 setup(
-        name="ouroboros",
-        version="0.1dev",
-        packages=["ouroboros"],
-        license="MIT License",
-        long_description=open("README.md").read(),
-        )
+    name="ouroboros",
+    version="0.1dev",
+    packages=["ouroboros"],
+    license="MIT License",
+    long_description=open("README.md").read(),
+)
