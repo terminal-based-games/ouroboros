ce4ce42d83073287289d3868baacbde55099ca6b
Carissa Allen <carissawood@outlook.com>
2019-07-12 21:11:50 -0700

    Write setup script
    Also testing Black formatting with this commit. Build should fail
    because this commit is not properly formatted according to Black
    standards.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+        name="ouroboros",
+        version="0.1dev",
+        packages=["ouroboros"],
+        license="MIT License",
+        long_description=open("README.md").read(),
+        )
