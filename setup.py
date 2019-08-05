7e86fb84e9c87428a86710eea3591855bf911da4
Carissa Allen <carissawood@outlook.com>
2019-08-04 23:20:53 -0700

    Update setup.py to fix setuptools issue
    Needed to import setuptools to the setup.py file to resolve "python
    setup.py egg_info" error.

--- setup.py
+++ setup.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 
+import pathlib
+import setuptools
 from distutils.core import setup
 
+# Directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# Package metadata
+NAME = "open-ouroboros"
+VERSION = "0.1"
+DESCRIPTION = "A terminal-based snake game" 
+README = (HERE / "README.md").read_text()
+URL = "https://github.com/terminal-based-games/ouroboros"
+AUTHOR = "Carissa & Mack"
+EMAIL = "cwood@pdx.edu"
+
+# Call to setup() which does all the work
 setup(
-    name="ouroboros",
-    version="0.1dev",
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=README,
+    url=URL,
+    author=AUTHOR,
+    author_email=EMAIL,
+    setup_requires=['wheel'],
     packages=["ouroboros"],
     license="MIT License",
-    long_description=open("README.md").read(),
 )
