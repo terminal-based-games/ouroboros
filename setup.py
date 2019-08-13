e2c6f798a16167423ed5a13caef06e4fa4cb6759
Carissa Allen <carissawood@outlook.com>
2019-08-13 08:28:01 -0700

    Fix setup.py

--- setup.py
+++ setup.py
@@ -18,15 +18,15 @@ EMAIL = "cwood@pdx.edu"
 
 # Call to setup() which does all the work
 setup(
-    name=NAME,
-    version=VERSION,
-    packages=["ouroboros"],
-    description=DESCRIPTION,
-    long_description=README,
-    url=URL,
-    author=AUTHOR,
-    author_email=EMAIL,
-    setup_requires=["wheel"],
-    entry_points={"console_scripts": ["ouroboros=ouroboros.__main__:main"]},
-    license="MIT License",
-)
+        name=NAME,
+        version=VERSION,
+        packages=["ouroboros"],
+        description=DESCRIPTION,
+        long_description=README,
+        url=URL,
+        author=AUTHOR,
+        author_email=EMAIL,
+        setup_requires=["wheel"],
+        entry_points={"console_scripts": ["ouroboros=ouroboros.__main__:main"]},
+        license="MIT License",
+        )
